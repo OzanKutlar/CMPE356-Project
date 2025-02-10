7bab898c450f41e0f1e59d2bac41c64ca2123a91
Ozan <ozankutlar@protonmail.com>
2025-02-10 11:16:57 +0300

    Added template files.

---
+++ src/main/java/cmpe/project/Project/CmpeProjectApplication.java
@@ -0,0 +1,13 @@
+package cmpe.project.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmpeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmpeProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cmpe/project/Project/CmpeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package cmpe.project.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmpeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
