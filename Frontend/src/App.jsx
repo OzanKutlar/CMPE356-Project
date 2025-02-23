2e660aa66a08453a433f1049113b4b0b26ef276c
Ozan <ozankutlar@protonmail.com>
2025-02-23 22:25:24 +0300

    Added stuff

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Email from "./components/EmailPopup/Email";
 import Header from "./components/Header/Header";
 import Slider from "./components/SlideShow/Slider";
 import Util from './Util'
+import "./App.css"; // Import styles
 
 export default function App() {
   return (
