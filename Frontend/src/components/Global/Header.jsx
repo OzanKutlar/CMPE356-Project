3feb359c5659a0abecd1092f9941302da425ea81
Ozan <ozankutlar@protonmail.com>
2025-03-03 10:45:38 +0300

    Update Header component with new Rose color scheme

--- Frontend/src/components/Global/Header.jsx
+++ Frontend/src/components/Global/Header.jsx
@@ -10,16 +10,16 @@ const Header = () => {
 
   return (
       <>
-        <header className="bg-red-300 flex justify-between items-center p-2 relative">
+        <header className="bg-rose-500 flex justify-between items-center p-2 relative">
           {/* Navigation Button */}
           <button
-              className="w-10 h-10 flex justify-center items-center text-2xl rounded-lg bg-red-800 text-pink-200 cursor-pointer transition-all duration-300 hover:bg-red-600 hover:text-white ml-3 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+              className="w-10 h-10 flex justify-center items-center text-2xl rounded-lg bg-rose-50 text-rose-700 cursor-pointer transition-all duration-300 hover:bg-red-600 hover:text-white ml-3 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
               onClick={() => setShowNavbar(!showNavbar)}
           >
             ☰
           </button>
           {/* Title */}
-          <h1 className="text-3xl font-bold text-black text-center flex-grow">
+          <h1 className="text-3xl font-bold text-red-200 text-center flex-grow">
             E-Butcher
           </h1>
           {/* Login Button or User Profile */}
@@ -27,7 +27,7 @@ const Header = () => {
               <UserProfile/>
           ) : (
               <button
-                  className="w-20 h-10 bg-red-800 text-pink-200 rounded-3xl cursor-pointer transition-all duration-300 hover:bg-red-600 hover:text-white mr-3 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+                  className="w-20 h-10 bg-rose-50 text-rose-700 rounded-3xl cursor-pointer transition-all duration-300 hover:bg-red-600 hover:text-white mr-3 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
                   onClick={() => setShowLogin(true)}
               >
                 Login
