5dd0e9d6e10321c93fdc85218fc3107c9f893ef2
Ozan <ozankutlar@protonmail.com>
2025-03-06 22:52:24 +0300

    ADded card no verification.

--- Frontend/src/components/CartPage/OrderForm.jsx
+++ Frontend/src/components/CartPage/OrderForm.jsx
@@ -1,38 +1,61 @@
 import React from 'react';
 
-const OrderForm = ({ formData, onFormDataChange }) => {
+const OrderForm = ({formData, onFormDataChange}) => {
 
     const handleChange = (e) => {
-        const { name, value } = e.target;
+        const {name, value} = e.target;
 
         if (name === 'cardNumber') {
             // Format card number with spaces every 4 digits
             const formattedValue = value.replace(/\D/g, '').replace(/(\d{4})(?=\d)/g, '$1 ');
-            onFormDataChange({ ...formData, [name]: formattedValue });
+            if (isValidCardNumber(formattedValue.replace(/\s+/g, ''))) {
+                onFormDataChange({...formData, [name]: formattedValue, cardNumberValid: true});
+            } else {
+                onFormDataChange({...formData, [name]: formattedValue, cardNumberValid: false});
+            }
         } else if (name === 'expiryDate') {
             // Allow only numbers in expiry date
             let formattedValue = value.replace(/\D/g, '').replace(/(\d{2})(?=\d)/g, '$1/');
 
-            if(value.length === 2 || value.length === 5){
+            if (value.length === 2 || value.length === 5) {
                 const [monthStr, yearStr] = value.replace(/\D/g, '').replace(/(\d{2})(?=\d)/g, '$1/').split('/');
                 const month = Number(monthStr);
                 const formattedMonth = month ? Math.min(month, 12).toString().padStart(2, '0') : '01';
 
-                if(value.length === 5) {
+                if (value.length === 5) {
                     const year = Number(yearStr);
                     const currentYear = new Date().getFullYear() % 100;
 
                     const formattedYear = year ? Math.max(year, currentYear).toString().slice(-2) : (currentYear + 1).toString().slice(-2);
                     formattedValue = `${formattedMonth}/${formattedYear}`;
-                }
-                else{
+                } else {
                     formattedValue = `${formattedMonth}`;
                 }
             }
-            onFormDataChange({ ...formData, [name]: formattedValue });
+            onFormDataChange({...formData, [name]: formattedValue});
         } else {
-            onFormDataChange({ ...formData, [name]: value });
+            onFormDataChange({...formData, [name]: value});
         }
+    };
+
+    const isValidCardNumber = (cardNumber) => {
+        let sum = 0;
+        let shouldDouble = false;
+        for (let i = cardNumber.length - 1; i >= 0; i--) {
+            let digit = parseInt(cardNumber.charAt(i));
+
+            if (shouldDouble) {
+                digit *= 2;
+                if (digit > 9) {
+                    digit -= 9;
+                }
+            }
+
+            sum += digit;
+            shouldDouble = !shouldDouble;
+        }
+
+        return (sum % 10) === 0;
     };
 
 
@@ -108,9 +131,6 @@ const OrderForm = ({ formData, onFormDataChange }) => {
                     className="w-full px-0 py-0 shadow-md shadow-red-500/50 rounded focus:ring focus:ring-red-300"
                 ></textarea>
             </div>
-            <button type="submit" className="bg-red-500 text-white font-bold py-2 px-4 rounded hover:bg-red-700">
-                Submit Order
-            </button>
         </form>
     );
 };
