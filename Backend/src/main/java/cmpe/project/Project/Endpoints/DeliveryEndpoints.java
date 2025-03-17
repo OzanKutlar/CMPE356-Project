bbb708f2366e3bf323b0b7e5470a6e14f2e1854f
Aviosche <cag.batuker@gmail.com>
2025-03-17 11:52:48 +0300

    Added Order and Delivery parts - DeliveryEndpoints for buttons - Order entity - Both are incomplete yet!

---
+++ Backend/src/main/java/cmpe/project/Project/Endpoints/DeliveryEndpoints.java
@@ -0,0 +1,16 @@
+package cmpe.project.Project.Endpoints;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+
+@RestController
+@RequestMapping("/api/delivery")
+public class DeliveryEndpoints {
+    
+}
---
+++ Backend/src/main/java/cmpe/project/Project/Entities/Order.java
@@ -0,0 +1,16 @@
+package cmpe.project.Project.Entities;
+
+import java.util.ArrayList;
+
+public class Order {
+    private int orderId;
+    private String pickUpAddress;
+    private String destinationAddress;
+    private ArrayList<String> content;
+    private float totalPrice;
+
+    
+
+
+
+}
